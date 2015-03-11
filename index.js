68742de5486476f2a6aae4f9f276fb338518ebb7
Maksim Talochka <maksim.tolo@gmail.com>
2015-03-11 22:01:41 +0300

    partial realization beckend

--- index.js
+++ index.js
@@ -3,30 +3,34 @@ var app = express();
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 var port = process.env.PORT || 8080;
 
-var users = require('./controllers/users')
+var routes = require('./controllers/index');
+//var users = require('./controllers/users')
 
-mongoose.connect('mongodb://localhost:27017/test');
+//mongoose.connect('mongodb://localhost:27017/test');
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(morgan('dev'));
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'true'}));
 
 // Middlewares, которые должны быть определены до passport:
-app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.session({ secret: 'SECRET' }));
+app.use(cookieParser());
+//app.use(express.session({ secret: 'SECRET' }));
  
 // Passport:
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use('/', routes);
 
 passport.use(new LocalStrategy({
 	usernameField: 'email',
