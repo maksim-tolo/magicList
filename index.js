9beb5103daac464f0d87af1cae2fb9c778fa4fb3
Maksim Talochka <maksim.tolo@gmail.com>
2015-03-11 22:07:25 +0300

    fix port

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ passport.use(new LocalStrategy({
 	});
 }));
 
-app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address
   var port = server.address().port
   console.log('App listening at http://%s:%s', host, port)
